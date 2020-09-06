f6b8c96f4ff57b8b36d29a85b25384e11c4cbeb5
János Kulcsár <janoskulcsar@bbv.ch>
2020-09-06 16:24:58 +0200

    Finish User repository tests

--- src/auth/user.repository.spec.ts
+++ src/auth/user.repository.spec.ts
@@ -1,8 +1,10 @@
 import { UserRepository } from './user.repository'
 import { Test } from '@nestjs/testing'
-import { InternalServerErrorException, ConflictException } from '@nestjs/common';
+import { InternalServerErrorException, ConflictException } from '@nestjs/common'
+import { User } from './user.entity'
+import * as bcrypt from 'bcryptjs';
 
-const mockCredentialDto = {username: 'TestUsername', password: 'TestPassword'}
+const mockCredentialDto = { username: 'TestUsername', password: 'TestPassword' }
 
 describe('UserRepository', () => {
   let userRepository
@@ -16,24 +18,74 @@ describe('UserRepository', () => {
   })
 
   describe('signUp', () => {
-      let save
-      beforeEach( () => {
-          save = jest.fn()
-          userRepository.create = jest.fn().mockReturnValue({save})
-      })
+    let save
+    beforeEach(() => {
+      save = jest.fn()
+      userRepository.create = jest.fn().mockReturnValue({ save })
+    })
     it('successfully signs up the user', () => {
-        save.mockResolvedValue(undefined)
-        expect(userRepository.signUp(mockCredentialDto)).resolves.not.toThrow()
+      save.mockResolvedValue(undefined)
+      expect(userRepository.signUp(mockCredentialDto)).resolves.not.toThrow()
     })
 
     it('throws a conflict exception as username already exsits', () => {
-        save.mockRejectedValue({code: '23505'})
-        expect(userRepository.signUp(mockCredentialDto)).rejects.toThrow(ConflictException)
+      save.mockRejectedValue({ code: '23505' })
+      expect(userRepository.signUp(mockCredentialDto)).rejects.toThrow(
+        ConflictException,
+      )
     })
 
     it('throws a conflict exception as username already exsits', () => {
-        save.mockRejectedValue({code: '23505'})
-        expect(userRepository.signUp(mockCredentialDto)).rejects.toThrow(InternalServerErrorException)
+      save.mockRejectedValue({ code: '23505' })
+      expect(userRepository.signUp(mockCredentialDto)).rejects.toThrow(
+        InternalServerErrorException,
+      )
     })
+  })
+
+  describe('validate User password', () => {
+    let user
+
+    beforeEach(() => {
+      userRepository.findOne = jest.fn()
+      user = new User()
+      user.username = 'TestUsername'
+      user.validatePassword = jest.fn()
+    })
+    it('returns the username as validation is successful', async () => {
+        userRepository.findOne.mockResolvedValue(user)
+        user.validatePassword.mockResolvedValue(true)
+
+        const result = await userRepository.validateUserPassword(mockCredentialDto)
+        expect(result).toEqual('TestUsername')
+    })
+
+    it('returns null as user cannot be found', async () => {
+        userRepository.findOne.mockResolvedValue(null)
+        const result = await userRepository.validateUserPassword(mockCredentialDto)
+        expect(user.validatePassword).not.toHaveBeenCalled()
+        expect(result).toBeNull()
+
+    })
+    it('returns null as password is invalid', async () => {
+        userRepository.findOne.mockResolvedValue(user)
+        user.validatePassword.mockResolvedValue(false)
+
+        const result = await userRepository.validateUserPassword(mockCredentialDto)
+        expect(user.validatePassword).toHaveBeenCalled()
+        expect(result).toBeNull()
+    })
+  })
+
+  describe('hashPassword', () => {
+      it('calls bcrypt.hash to generate a hash', async () => {
+          bcrypt.hash = jest.fn().mockResolvedValue('testHash')
+          expect(bcrypt.hash).not.toHaveBeenCalled()
+
+          const result = await userRepository.hashPassword('testPassword', 'testSalt')
+
+          expect(bcrypt.hash).toHaveBeenCalledWith('testPassword', 'testSalt')
+          expect(result).toEqual('testHash')
+      })
   })
 })
